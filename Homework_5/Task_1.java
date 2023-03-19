ce676b56702547da12c3b89caf509314b51a84fc
Sergey Mysatov <smysatov@gmail.comm>
2023-03-19 19:47:17 +0300

    Seminar_5

---
+++ Homework_5/Task_1.java
@@ -0,0 +1,24 @@
+package Homework_5;
+
+import java.util.List;
+import java.util.Map;
+
+public class Task_1 {
+    /**
+ * Есть тест. Нужно извлечь из него все слова и отсортировать по длине.
+ * Мороз и солнце день чудесный Еще ты дремлешь друг прелестный Пора красавица проснись
+ *
+ * и
+ * ты
+ * Еще
+ * день друг Пора
+ * ..
+ *
+ */
+    public static void main(String[] args) {
+        
+        static void collectStats(String text) {
+            Map<Integer, List<String>> stats = new HashMap<>();
+        }
+    }
+}
---
+++ Seminar_5/Task_2.java
@@ -0,0 +1,7 @@
+package Seminar_5;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        
+    }
+}
